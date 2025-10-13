592a759e2c3a9889632ef6bcc320d7e37c900fb3
TM-Luan <minhluandz303@gmail.com>
2025-10-13 09:59:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# khoa_luan_tot_ngiep_gia_su_nguoi_hoc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/khoa_luan_tot_ngiep_gia_su_nguoi_hoc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.khoa_luan_tot_ngiep_gia_su_nguoi_hoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
